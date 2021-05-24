b311cb29fc39bbd1c870d16220ba5faa78942ee4
Alexey Lebedev <alebedev.exceedteam@gmail.com>
2021-05-24 13:56:13 +0300

    create application.Init app

---
+++ 3.MVC/background.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+  el: "#body",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 1000.00,
+  minWidth: 1200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x7307d,
+  shininess: 150.00,
+  waveHeight: 40.00,
+  waveSpeed: 1.70,
+  zoom: 0.65
+})
