aa77960fcd18878e68ad68a967f447caec73c6b5
Alexey Lebedev <alebedev.exceedteam@gmail.com>
2021-05-31 10:26:14 +0300

    added database

--- 3.MVC/Node-JS/app.js
+++ 3.MVC/Node-JS/app.js
@@ -1,12 +1,32 @@
-const express = require('express');
+import express from 'express';
+import { Schema as _Schema, connect, model } from 'mongoose';
 const app = express();
+const Schema = _Schema;
+
+const taskScheme = new Schema({
+  text: String,
+  isCheck: Boolean
+});
+
+const url = 'mongodb+srv://Alexey:restart987@cluster0.nsbyf.mongodb.net/TestEducationBD?retryWrites=true&w=majority'
+connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Task = model('tasks', taskScheme);
 
 app.get('/', (req, res) => {
-  res.send('Hello people');
+  const task = new Task({
+    text: 'First task',
+    isCheck: false
+  });
+  task.save().then(result => {
+    res.send(result);
+  }).catch(err => console.log(err))
 });
 
 app.get('/paramRequest', (req, res) => {
-  res.send(req.query);
+  Task.find().then(result => {
+    res.send({data:result});
+  });
 });
 
 app.listen(8800, () => {
