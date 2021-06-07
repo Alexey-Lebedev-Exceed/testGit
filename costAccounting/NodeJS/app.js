dfb0cf24ca180c61dbe5d6c880b85a8c4ecebbdf
Alexey Lebedev <alebedev.exceedteam@gmail.com>
2021-06-07 17:41:24 +0300

    cost accounting project created

---
+++ costAccounting/NodeJS/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require ('cors');
+const mongoose = require('mongoose');
+
+const apiRoutes = require('./src/modules/routes/routes');
+
+app.use(cors());
+app.use(express.json());
+app.use("/", apiRoutes);
+
+const url = 'mongodb+srv://Alexey:restart987@cluster0.nsbyf.mongodb.net/TestCostAccounting?retryWrites=true&w=majority'
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.listen(7777, () => {
+  console.log('Example app listening on port 7777');
+});
