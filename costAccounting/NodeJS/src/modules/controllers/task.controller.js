adfef1c2af694edc843a71044bd9a4a9a9c0258f
Alexey Lebedev <alebedev.exceedteam@gmail.com>
2021-06-16 15:59:13 +0300

    added fixes

--- costAccounting/NodeJS/src/modules/controllers/task.controller.js
+++ costAccounting/NodeJS/src/modules/controllers/task.controller.js
@@ -3,7 +3,7 @@ const Task = require('../../db/models/task/index');
 
 module.exports.getAllTasks = ((req, res) => {
   if(!req.body){
-    console.log('Данные не верны')
+    res.send('Данные не верны')
   } else {
     Task.find().then(result => {
       res.send(result);
@@ -20,7 +20,7 @@ module.exports.createNewTask = ((req, res) => {
 
 module.exports.changeTaskInfo = ((req, res) => {
   if(!req.body._id || req.body.text && req.body.isCheck){
-    console.log('Данные не верны')
+    res.send('Данные не верны')
   } else {
     Task.updateOne({_id: req.body._id}, req.body).then(result => {
       Task.find().then(result => {
@@ -32,7 +32,7 @@ module.exports.changeTaskInfo = ((req, res) => {
 
 module.exports.deleteTask = ((req, res) => {
   if(!req.body._id){
-    console.log('Данные не верны')
+    res.send('Данные не верны')
   } else {
     Task.deleteOne({_id: req.query._id}).then(result => {
       res.send(result)
